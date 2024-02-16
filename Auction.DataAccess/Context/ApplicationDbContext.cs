ecf7c8f6059a0d14a00719d0649337692efd6943
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-16 10:10:59 +0300

    Migration db

---
+++ Auction.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Auction.DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.DataAccess.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
