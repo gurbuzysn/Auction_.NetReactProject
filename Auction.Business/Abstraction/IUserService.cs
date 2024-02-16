37b77fee9bddce520715f658a5ebda7f60f4618c
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-16 12:56:26 +0300

    .

---
+++ Auction.Business/Abstraction/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.Business.Abstraction
+{
+    public interface IUserService
+    {
+        Task<ApiResponse> Register(RegisterRequestDto model);
+        Task<ApiResponse>
+    }
+}
