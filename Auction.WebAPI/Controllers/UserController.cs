2d8a1fb7239129c65460c6b27417eede99edb1c2
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-17 18:01:11 +0300

    .

---
+++ Auction.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Auction.Business.Abstraction;
+using Auction.Business.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auction.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> CreateUser([FromBody]RegisterRequestDto model)
+        {
+            var response = await _userService.Register(model);
+            if(response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginUser ([FromBody] LoginRequestDto model)
+        {
+            var response = await _userService.Login(model);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response);
+        }
+
+
+    }
+}
--- Auction.WebAPI/Program.cs
+++ Auction.WebAPI/Program.cs
@@ -1,9 +1,20 @@
+using Auction.Business.Abstraction;
+using Auction.Business.Concrete;
+using Auction.Core.Models;
 using Auction.DataAccess.Context;
+using Auction.DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped(typeof(ApiResponse));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
