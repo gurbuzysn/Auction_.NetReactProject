3051bcf3528027ba437bb1127ff90ac52406ccd8
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-17 15:55:57 +0300

    .

--- Auction.Business/Concrete/UserService.cs
+++ Auction.Business/Concrete/UserService.cs
@@ -2,6 +2,7 @@
 using Auction.Business.Dtos;
 using Auction.Core.Models;
 using Auction.DataAccess.Context;
+using Auction.DataAccess.Enums;
 using Auction.DataAccess.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
@@ -19,12 +20,12 @@ namespace Auction.Business.Concrete
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<ApplicationUser> _roleManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly ApiResponse _response;
         private string secretKey;
         
 
-        public UserService(ApplicationDbContext context, IMapper mapper, UserManager<ApplicationUser> userManager, RoleManager<ApplicationUser> roleManager, ApiResponse response, IConfiguration configuration)
+        public UserService(ApplicationDbContext context, IMapper mapper, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApiResponse response, IConfiguration configuration)
         {
             _context = context;
             _mapper = mapper;
@@ -53,6 +54,42 @@ namespace Auction.Business.Concrete
             }
 
             var newUser = _mapper.Map<ApplicationUser>(model);
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+
+            if (result.Succeeded)
+            {
+                var isTrue = _roleManager.RoleExistsAsync(UserType.Administrator.ToString()).GetAwaiter().GetResult();
+
+                if (!_roleManager.RoleExistsAsync(UserType.Administrator.ToString()).GetAwaiter().GetResult())
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(UserType.Administrator.ToString()));
+                    await _roleManager.CreateAsync(new IdentityRole(UserType.Seller.ToString()));
+                    await _roleManager.CreateAsync(new IdentityRole(UserType.NormalUser.ToString()));
+                }
+
+                if(model.UserType.ToString().ToLower() == UserType.Administrator.ToString().ToLower())
+                {
+                    await _userManager.AddToRoleAsync(newUser, UserType.Administrator.ToString());
+                }
+                if (model.UserType.ToString().ToLower() == UserType.Seller.ToString().ToLower())
+                {
+                    await _userManager.AddToRoleAsync(newUser, UserType.Seller.ToString());
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(newUser, UserType.NormalUser.ToString());
+                }
+
+                _response.StatusCode = System.Net.HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return _response;
+            }
+
+                foreach (var error in result.Errors)
+                {
+                    _response.ErrorMessages.Add(error.ToString());
+                }
+                return _response;
 
         }
     }
