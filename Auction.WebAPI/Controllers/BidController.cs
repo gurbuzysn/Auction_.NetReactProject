cd6da4d84a495174e5c54257247f1ac79ed2aba7
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-27 21:28:02 +0300

    BidController

---
+++ Auction.WebAPI/Controllers/BidController.cs
@@ -0,0 +1,97 @@
+﻿using Auction.Business.Abstraction;
+using Auction.Business.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auction.WebAPI.Controllers
+{
+    public class BidController : Controller
+    {
+        private readonly IBidService _bidService;
+
+        public BidController(IBidService bidService)
+        {
+            _bidService = bidService;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateBid(CreateBidDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _bidService.CreateBid(model);
+
+                if (!response.IsSuccess)
+                {
+                    return BadRequest(response);
+                }
+                return Ok(response);
+            }
+            return BadRequest();
+        }
+
+
+        [HttpGet("bidId:int")]
+        public async Task<IActionResult> GetBidById(int bidId)
+        {
+            var response = await _bidService.GetBidById(bidId);
+
+            if (!response.IsSuccess)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpGet("bidId:int")]
+        public async Task<IActionResult> ModifyBid(int bidId, UpdateBidDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _bidService.UpdateBid(bidId, model);
+
+                if (!response.IsSuccess)
+                {
+                    return BadRequest(response);
+                }
+                return Ok(response);
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> AutoBid(CreateBidDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _bidService.AutomaticliyCreateBid(model);
+
+                if (!response.IsSuccess)
+                {
+                    return BadRequest(response);
+                }
+                return Ok(response);
+            }
+            return BadRequest();
+        }
+
+
+
+        [HttpGet("GetBidsByVehicle/{bidId:int}")]
+        public async Task<IActionResult> GetBidByVehicle(int bidId)
+        {
+            var response = await _bidService.GetBidByVehicleId(bidId);
+
+            if (!response.IsSuccess)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+
+
+
+
+    }
+}
